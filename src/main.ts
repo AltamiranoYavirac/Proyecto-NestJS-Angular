8b5f22d9e907fc3491a0f0d438443e60c2833435
AltamiranoYavirac <cav.altamirano@yavirac.edu.ec>
2024-11-17 09:52:33 -0500

    Ultimos cambios clase 8

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
 
   //swagger
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Gatos example')
+    .setDescription('The Gatos API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Gatos')
     .build();
   const document = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
